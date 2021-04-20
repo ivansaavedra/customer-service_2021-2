e76dc383236024af9bd006708f33cc9239e3d4b6
Ivan <ivan.saavedra@ciencias.unam.mx>
2021-04-19 20:14:47 -0500

    feature CRUD Region implementation
    Se realizó toda la implementación del CRUD Region
    
    Responsable: Iván Saavedra
    API: Customer
    Sprint 1

---
+++ src/main/java/com/dws/customerservice/service/RegionServiceImp.java
@@ -0,0 +1,47 @@
+package com.dws.customerservice.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dws.customerservice.dto.Region;
+import com.dws.customerservice.dto.RespuestaApi;
+import com.dws.customerservice.repository.RegionRepository;
+
+@Service
+public class RegionServiceImp implements RegionService {
+	
+	@Autowired
+	RegionRepository regionRepository;
+
+	@Override
+	public List<Region> getRegions() {
+		return regionRepository.getRegions();
+	}
+
+	@Override
+	public Region getRegion(int id) {
+		// TODO Auto-generated method stub
+		return regionRepository.getRegion(id);
+	}
+
+	@Override
+	public RespuestaApi createRegion(Region region) {
+		// TODO Auto-generated method stub
+		return regionRepository.createRegion(region);
+	}
+
+	@Override
+	public RespuestaApi updateRegion(Region region, int id) {
+		// TODO Auto-generated method stub
+		return regionRepository.updateRegion(region, id);
+	}
+
+	@Override
+	public RespuestaApi deleteRegion(int id) {
+		// TODO Auto-generated method stub
+		return regionRepository.deleteRegion(id);
+	}
+
+}
